a7bdfe3ab2b19b8b2de89782505f10d48e5cbae4
KhamichenkaIrina <irinakhamichenka@mail.ru>
2023-09-16 20:50:21 +0300

    feat:: add sliderText  (Sat, Sep 16, 2023 08:50:21 PM)

--- index.js
+++ index.js
@@ -3,25 +3,33 @@ const sliderImagesWrapper = document.querySelector(".images__wrapper");
 const slidertImages = document.querySelectorAll(".images__slider__wrapper");
 const widthImage = slidertImages[0].offsetWidth;
 
+const textSliderWrapper = document.querySelector(".text__slider__wrapper");
+const sliderText = document.querySelectorAll(".text__slider");
+const widthText = sliderText[0].offsetWidth;
+
 const sliderButtons = document.querySelectorAll(".slider__button__wrapper");
 const windowWidth = window.innerWidth;
 
 let activeImageIndex = 0;
 
 const handleSliderMove = (index) => {
-  //   sliderButtons[activeImageIndex].classList.remove("active");
+  sliderButtons[activeImageIndex].classList.remove("active");
 
   activeImageIndex = index;
-  console.log("kjvhkjvk");
   sliderImagesWrapper.style.transform = `translateX(${
     -widthImage * activeImageIndex
   }px)`;
 
-  //   sliderButtons[activeImageIndex].classList.add("active");
+  textSliderWrapper.style.transform = `translateX(${
+    -widthText * activeImageIndex
+  }px)`;
+
+  sliderButtons[activeImageIndex].classList.add("active");
 };
 
 // кнопки
 sliderButtons.forEach(function (button, index) {
   button.addEventListener("click", () => handleSliderMove(index));
-  console.log("kjglkg");
 });
+
+//
